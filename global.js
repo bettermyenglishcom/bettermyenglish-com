061b35e57065099f3851671dde05be1de9c96efd
bettermyenglishcom <bettermyenglishcom@gmail.com>
2025-09-16 08:36:09 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,7 +1,7 @@
 const fiverrIcon = document.getElementById("fiverrIcon");
 const youtubeIcon = document.getElementById("youtubeIcon");
 
-fiverrIcon.addEventListener("click", function() { window.open("https://www.fiverr.com/sellers/bettermyenglish/"); });
+fiverrIcon.addEventListener("click", function() { window.open("https://www.fiverr.com/users/bettermyenglish/"); });
 
 youtubeIcon.addEventListener("click", function() { window.open("https://www.youtube.com/@bettermyenglish/"); });
 
