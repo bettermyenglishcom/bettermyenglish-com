b9fb39ab1131ecd8d8910a95480128162b097440
bettermyenglishcom <bettermyenglishcom@gmail.com>
2025-09-16 08:29:13 -0400

    Create global.js

---
+++ global.js
@@ -0,0 +1,7 @@
+const fiverrIcon = document.getElementById("fiverrIcon");
+const youtubeIcon = document.getElementById("youtubeIcon");
+
+fiverrIcon.addEventListener("click", function() { window.open("https://www.fiverr.com/sellers/bettermyenglish/"); });
+
+youtubeIcon.addEventListener("click", function() { window.open("https://www.youtube.com/@bettermyenglish/"); });
+
