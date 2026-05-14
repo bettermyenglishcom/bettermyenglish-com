458983422d9b56f55ea45fd1d7370e58ec997ca8
bettermyenglishcom <bettermyenglishcom@gmail.com>
2026-05-14 05:32:39 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,8 +1,38 @@
-const headerLogo = document.getElementById("headerLogo");
-const fiverrIcon = document.getElementById("fiverrIcon");
-const youtubeIcon = document.getElementById("youtubeIcon");
+const hyperlinked = document.getElementsByClassName("hyperlinked");
+for (const el of hyperlinked) { el.addEventListener("click", goTo); }
 
-headerLogo.addEventListener("click", function() { window.location.href = "https://bettermyenglish.com/"; });
-fiverrIcon.addEventListener("click", function() { window.open("https://www.fiverr.com/users/bettermyenglish/"); });
-youtubeIcon.addEventListener("click", function() { window.open("https://www.youtube.com/@bettermyenglish/?sub_confirmation=1"); });
+function goTo() {
 
+    switch (this.dataset.redirectLink) {
+
+        case ("0"):
+            window.location.href = "https://bettermyenglish.com/index.html";
+            break;
+        
+        case ("1"):
+            window.location.href = "https://bettermyenglish.com/about.html";
+            break;
+        
+        case ("2"):
+            window.location.href = "https://bettermyenglish.com/resources.html";
+            break;
+        
+        case ("3"):
+            window.open("https://youtube.com/@bettermyenglish/?sub_confirmation=1");
+            break;
+        
+        case ("4"):
+            window.open("https://discord.gg/yuE9Rzpkh");
+            break;
+        
+        case ("5"):
+            window.open("https://wa.me/message/TLRNWRKLTA7SM1");
+            break;
+
+        case("6"):
+            window.open("https://www.instagram.com/bettermyenglishcom/");
+            break;
+
+    }
+
+}
